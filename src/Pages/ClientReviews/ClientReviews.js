4716523ead93520726da8b4175014a971f217c80
majedulhaque1 <majedulhaquerakib601@gmail.com>
2022-05-04 21:32:09 +0600

    Client resview section done

--- src/Pages/ClientReviews/ClientReviews.js
+++ src/Pages/ClientReviews/ClientReviews.js
@@ -1,47 +1,62 @@
 import React from 'react';
+import './ClientReviews.css';
 import { Card, Col, Container, Row } from 'react-bootstrap';
 import imageClient from '../../images/gear.png';
+import {Swiper, SwiperSlide} from 'swiper/react';
+import {Navigation} from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
 const ClientReviews = () => {
     return (
-        <div>
-            <Container>
-                <Row>
-                    <Col xs={12} md={6} lg={4}>
-                        <Card>
-                            <Card.Img style={{width: '100px', height:'100px'}} variant='top' src={imageClient}>
+        <>
+            <Swiper
+            slidesPerView={1}
+            style={{height:'70vh', padding:'100px 0'}}
+            modules={[Navigation]}
+            navigation={true}
+            >
+                <SwiperSlide>
+                <Card>
+                            <Card.Img className='img-style shadow-lg' variant='top' src={imageClient}>
                             </Card.Img>
                             <Card.Body>
-                                <Card.Text>
+                                <Card.Text className='text-center'>
                                     your services is really awesome.
                                 </Card.Text>
                             </Card.Body>
                         </Card>
-                    </Col>
-                    <Col xs={12} md={6} lg={4}>
-                        <Card>
-                        <Card.Img style={{width: '100px', height:'100px'}} variant='top' src={imageClient}>
+                </SwiperSlide>
+                <SwiperSlide>
+                 <Card>
+                            <Card.Img className='img-style shadow-lg' variant='top' src={imageClient}>
                             </Card.Img>
-                            <Card.Body>
-                                <Card.Text>
+                           <Card.Body>
+                                <Card.Text className='text-center'>
+                                    your services is really awesome.
+                                    your services is really awesome.
                                     your services is really awesome.
                                 </Card.Text>
                             </Card.Body>
                         </Card>
-                    </Col>
-                    <Col xs={12} md={6} lg={4}>
-                        <Card>
-                        <Card.Img style={{width: '100px', height:'100px'}} variant='top' src={imageClient}>
+                </SwiperSlide>
+                <SwiperSlide>
+                <Card>
+                            <Card.Img className='img-style shadow-lg' variant='top' src={imageClient}>
                             </Card.Img>
                             <Card.Body>
-                                <Card.Text>
+                                <Card.Text className='text-center'>
+                                    your services is really awesome.
+                                    your services is really awesome.
+                                    your services is really awesome.
+                                    your services is really awesome.
                                     your services is really awesome.
                                 </Card.Text>
                             </Card.Body>
                         </Card>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
+                </SwiperSlide>
+            </Swiper>
+            
+        </>
     );
 };
 
