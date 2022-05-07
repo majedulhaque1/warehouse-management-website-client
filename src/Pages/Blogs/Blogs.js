b8ec85437d096e283e256b622717da12752055f0
majedulhaque1 <majedulhaquerakib601@gmail.com>
2022-05-08 00:16:20 +0600

    secound quesion and third question answer done

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './';
+import './Blogs.css';
 
 const Blogs = () => {
     return (
@@ -16,25 +16,32 @@ const Blogs = () => {
                 </p>
             </div>
             <div>
-                <h2>JavaScript Vs Node JS</h2>
-                <h3>JavaScript</h3>
+                <h2>When we use Node and mongodb?</h2>
+                <h3>When we use node js?</h3>
                 <p>
-                    this is question answer.this is question answer.this is question answer.this is question answer.this is question answer.this is question answer.this is question answer.
+                    Node js is a javascript runtime environment.Node js use for non-blocking.And node js nature is single treaded.Node js is very fast.Node js is lightweight and efficient.And node js use the asynchronous mode operations.We use node js when we develop Input/Output incentive task application.If if give example when a user enter the application and user give input and get output something
+                    that's why we use node js.Becuase node js can handle network application efficiently.That's why user get better exprence.
                 </p>
-                <h3>Node Js</h3>
+                <h3>When we use mongodb?</h3>
                 <p>
-                    this is question answer.this is question answer.this is question answer.this is question answer.this is question answer.this is question answer.this is question answer.
+                    Mongodb is NoSQL Database.Mongodb id non relational database.And mongodb flexible for develepers.We can store data in mongodb JSON format.Mongodb is flexible and highly ableable for internet application.That why we use mongodb.when we need mongodb.when we need to flexibility and we dont't have many time for build application.
                 </p>
             </div>
             <div>
-                <h2>JavaScript Vs Node JS</h2>
-                <h3>JavaScript</h3>
+                <h2>Defference between SQL and NoSQL?</h2>
+                <h3>What is SQL Database?</h3>
                 <p>
-                    this is question answer.this is question answer.this is question answer.this is question answer.this is question answer.this is question answer.this is question answer.
+                    <p>1. SQL database is Structure query language.</p>
+                    <p>2. It is relational database system.</p>
+                    <p>3. SQL Database vericaly scaleable.</p>
+                    <p>4. It is table based database.</p>
                 </p>
-                <h3>Node Js</h3>
+                <h3>What is NoSQL database?</h3>
                 <p>
-                    this is question answer.this is question answer.this is question answer.this is question answer.this is question answer.this is question answer.this is question answer.
+                    <p>1. NoSQL database is not only Structure query language.</p>
+                    <p>2. It is non relational database system.</p>
+                    <p>3. NoSQL database horizentaly scaleabale.</p>
+                    <p>1. It is docoment based database or JSON.</p>
                 </p>
             </div>
             <div>
