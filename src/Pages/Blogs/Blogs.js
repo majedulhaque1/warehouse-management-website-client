865ba24be71eeb86da38009edc52b48a144c4513
majedulhaque1 <majedulhaquerakib601@gmail.com>
2022-05-08 01:38:09 +0600

    fourth question answer done and some style done

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -4,8 +4,8 @@ import './Blogs.css';
 const Blogs = () => {
     return (
         <div className='d-flex justify-content-between flex-wrap px-5 w-100'>
-            <div>
-                <h2>Defferance between JavaScript And Node JS?</h2>
+            <div className='blog-item shadow-lg p-3 mt-4'>
+                <h2 className='text-warning'>Defferance between JavaScript And Node JS?</h2>
                 <h3>JavaScript</h3>
                 <p>
                     JavaScript is a programming language.And JavaScript is high level and interprete programming language.JavaScript can run only browsers.JavaScript used for client side.And JavaScript used for front end development.JavaScipt is written in c++.
@@ -15,8 +15,8 @@ const Blogs = () => {
                     Node JS is a javascript runtime environment.We can run javascript outside of the browser with the help of Node JS Runtime.We use Node Js for Back End Development.Node js is Written in c,c++ and JavaScript.
                 </p>
             </div>
-            <div>
-                <h2>When we use Node and mongodb?</h2>
+            <div className='blog-item shadow-lg p-3 mt-4'>
+                <h2 className='text-warning'>When we use Node and mongodb?</h2>
                 <h3>When we use node js?</h3>
                 <p>
                     Node js is a javascript runtime environment.Node js use for non-blocking.And node js nature is single treaded.Node js is very fast.Node js is lightweight and efficient.And node js use the asynchronous mode operations.We use node js when we develop Input/Output incentive task application.If if give example when a user enter the application and user give input and get output something
@@ -27,13 +27,13 @@ const Blogs = () => {
                     Mongodb is NoSQL Database.Mongodb id non relational database.And mongodb flexible for develepers.We can store data in mongodb JSON format.Mongodb is flexible and highly ableable for internet application.That why we use mongodb.when we need mongodb.when we need to flexibility and we dont't have many time for build application.
                 </p>
             </div>
-            <div>
-                <h2>Defference between SQL and NoSQL?</h2>
+            <div className='blog-item shadow-lg p-3 mt-4'>
+                <h2 className='text-warning'>Defference between SQL and NoSQL?</h2>
                 <h3>What is SQL Database?</h3>
                 <p>
                     <p>1. SQL database is Structure query language.</p>
                     <p>2. It is relational database system.</p>
-                    <p>3. SQL Database vericaly scaleable.</p>
+                    <p>3. SQL Database verticaly scaleable.</p>
                     <p>4. It is table based database.</p>
                 </p>
                 <h3>What is NoSQL database?</h3>
@@ -44,26 +44,10 @@ const Blogs = () => {
                     <p>1. It is docoment based database or JSON.</p>
                 </p>
             </div>
-            <div>
-                <h2>JavaScript Vs Node JS</h2>
-                <h3>JavaScript</h3>
+            <div className='blog-item shadow-lg p-3 mt-4'>
+                <h2 className='text-warning'>What is the purpose of JWT?</h2>
                 <p>
-                    this is question answer.this is question answer.this is question answer.this is question answer.this is question answer.this is question answer.this is question answer.
-                </p>
-                <h3>Node Js</h3>
-                <p>
-                    this is question answer.this is question answer.this is question answer.this is question answer.this is question answer.this is question answer.this is question answer.
-                </p>
-            </div>
-            <div>
-                <h2>JavaScript Vs Node JS</h2>
-                <h3>JavaScript</h3>
-                <p>
-                    this is question answer.this is question answer.this is question answer.this is question answer.this is question answer.this is question answer.this is question answer.
-                </p>
-                <h3>Node Js</h3>
-                <p>
-                    this is question answer.this is question answer.this is question answer.this is question answer.this is question answer.this is question answer.this is question answer.
+                    JWT JSON web token.JWT used to share security information client to server.Each JSON web token contains encoded JSON object.And JWT are using cryptographic algorithom.We can secure data and secure application.
                 </p>
             </div>
         </div>
